4e491e267981e0b53053de973aa92debf63e3ef4
AAllred18 <161763678+AAllred18@users.noreply.github.com>
2024-12-10 16:16:47 -0700

    Update index.js with get and post for add

--- index.js
+++ index.js
@@ -43,4 +43,44 @@ app.get("/Login", (req, res) => {
     res.render("login");
 });
 
+// Route to populate add Log dropdown
+app.get('/addLog', (req, res) => {
+    const { success } = req.query; 
+    // Fetch types to populate the dropdown
+    knex('activities')
+      .select('activity_id', 'activity_description')
+      .then(activity_types => {
+            // Render the add form with the activity type
+            res.render('addLog', { activity_types });
+        })
+
+        .catch(error => {
+            console.error('Error fetching activity types:', error);
+            res.status(500).send('Internal Server Error');
+    });
+});
+
+// Route to Create new log
+app.post('/addLog', (req, res) => {
+    // Extract form values from req.body
+    const activity = req.body.activity; // Default to empty string if not provided; 
+    const date = req.body.date; 
+    const notes = req.body.notes || ''; // Default to empty string if not provided
+  
+    // Insert the new log into the database
+    knex('BabyLog')
+      .insert({
+       activity,
+       date,
+       notes
+      })
+      .then(() => {
+          res.redirect('/viewbabyLog'); // Redirect to the Home Baby Log page after adding
+      })
+      .catch(error => {
+          console.error('Error adding Log', error);
+          res.status(500).send('Internal Server Error');
+      });
+ });
+
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
