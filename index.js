a3f8010550706061094fa9ee2ab282201eaf8e52
Gavin Smith <gavinjacobsmith@gmail.com>
2024-12-10 16:35:05 -0700

    linking the styles.css sheet / making a few modifications to the files

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ let app = express();
 
 let path = require("path");
 
-
 const port = 3000;
-
 
 app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, "public") ));
 
 
 const knex = require("knex") ({
@@ -21,7 +21,7 @@ const knex = require("knex") ({
         host : "localhost",
         user : "testuser",
         password : "test",
-        database : "assignment 3a",
+        database : "assignment3a",
         port : 5432
     }
 })
@@ -35,12 +35,5 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.get("/", (req, res) => {
-    res.send("Welcome to the Home Page!");
-});
-
-app.get("/Login", (req, res) => {
-    res.render("login");
-});
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
